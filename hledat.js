1975787b785aa25760b7d9f0c85bffb26f40710e
Dominik Hejl <hejl@faster.cz>
2025-02-18 12:20:19 +0100

    improve search results layout

--- hledat.js
+++ hledat.js
@@ -26,7 +26,7 @@ function searchData() {
 
     if (filteredResults.length > 0) {
         filteredResults.forEach(item => {
-            resultsDiv.innerHTML += `<div class="col-4 p-3 h-100"><div class="bg-frostedglass p-4 text-center"><b>${item.name}</b><img src="${item.img}"/><p>${item.desc}</p></div></div>`; // Change 'name' to the appropriate key
+            resultsDiv.innerHTML += `<div class="col-4 p-3 h-100"><div class="bg-frostedglass p-4 text-center"><b>${item.name}</b><img src="${item.img}" height="80" class="p-2"/><p>${item.desc}</p></div></div>`; // Change 'name' to the appropriate key
         });
     } else {
         resultsDiv.innerHTML = '<div class="col-4 p-3"><div class="bg-frostedglass p-4 text-center"><p>Nic jsme nenašli 😔</p></div></div>';

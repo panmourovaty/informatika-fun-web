36c9f71f27b6396fa5afbb014d693ca1f70b072d
Dominik Hejl <hejl@faster.cz>
2025-02-19 15:54:11 +0100

    rework sidebar

--- skript.js
+++ skript.js
@@ -1,4 +1,17 @@
-function toggleSidebar() {
-    let sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("show");
+const sidebar = document.getElementById('sidebar');
+const toggleBtn = document.getElementById('toggle-btn');
+
+function checkScreenSize() {
+    if (window.innerWidth >= 1024) {
+        sidebar.classList.add('open');
+    } else {
+        sidebar.classList.remove('open');
+    }
 }
+
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+});
+
+window.addEventListener('resize', checkScreenSize);
+checkScreenSize();
